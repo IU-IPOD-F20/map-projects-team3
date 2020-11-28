4e9e280772b024555ad88d8b9371192b031355ef
picroc <picroc@yandex-team.ru>
2020-11-29 01:02:23 +0300

    feat: Add main project structure

--- net-worth-tracker/src/index.tsx
+++ net-worth-tracker/src/index.tsx
@@ -1,17 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Router } from 'react-router';
+import { createHashHistory } from 'history';
+import { ApiContext } from './context';
+import { Api } from './services';
+
+const history = createHashHistory();
+const api = new Api();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Router history={history}>
+			<ApiContext.Provider value={api}>
+				<App />
+			</ApiContext.Provider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ net-worth-tracker/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+export const MainPage: FC = () => {
+	return (
+		<div>
+			Welcome! Just a test page, there will
+			be more here in future
+		</div>
+	);
+};
