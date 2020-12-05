c5dc548c3cec1c3d7fb78d11f5b6a40a30be9a98
Prometheus <prometheus3375@gmail.com>
2020-12-05 16:13:05 +0300

    Add basic view and necessary models

--- nwt-backend/app/admin.py
+++ nwt-backend/app/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Record, RecordType
+
+admin.site.register(Record)
+admin.site.register(RecordType)
--- nwt-backend/app/models.py
+++ nwt-backend/app/models.py
@@ -1,1 +1,29 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import CASCADE, ForeignKey, Model
+
+
+# from enum import IntEnum, unique
+# @unique
+# class RecordType(IntEnum):
+#     ASSET = 1
+#     LIABILITY = 2
+
+
+class RecordType(Model):
+    name = models.CharField(max_length=10)
+
+    def __repr__(self):
+        return self.name
+
+
+class Record(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    date_time = models.DateTimeField('date & time')
+    name = models.CharField(max_length=200)
+    # type_ = models.PositiveSmallIntegerField('record type')
+    type = ForeignKey(RecordType, on_delete=CASCADE)
+    amount = models.PositiveIntegerField('amount')
+
+    def __repr__(self):
+        return f'Record({self.user_id}, {self.date_time}, {self.name}, {self.type}, {self.amount})'
