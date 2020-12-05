70cb049372e37efff4d160d1e9c7f81bcfb191a7
Prometheus <prometheus3375@gmail.com>
2020-12-05 14:17:43 +0300

    Initial backend commit

---
+++ nwt-backend/install.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.9
+python3.9 --version
+python3.9 -m venv venv
+./venv/bin/activate
+pip install -U pip
+pip install -U setuptools wheel
+pip install -r requirements.txt
