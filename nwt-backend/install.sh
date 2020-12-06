7f775520190a09b94988d8793255086fa465d638
Prometheus <prometheus3375@gmail.com>
2020-12-06 04:50:14 +0300

    Fix install.sh

--- nwt-backend/install.sh
+++ nwt-backend/install.sh
@@ -4,6 +4,6 @@ sudo apt install python3.9 python3.9-venv
 python3.9 --version
 python3.9 -m venv venv
 source ./venv/bin/activate
-pip install -U pip
-pip install -U setuptools wheel
-pip install -r requirements.txt
+pip3.9 install -U pip
+pip3.9 install -U setuptools wheel
+pip3.9 install -r requirements.txt
