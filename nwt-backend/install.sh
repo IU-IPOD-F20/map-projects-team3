0738e5686ec5029b14cf755d2d52a27adc96f679
Prometheus <prometheus3375@gmail.com>
2020-12-06 04:53:50 +0300

    Separate install.sh and init_venv.sh

--- nwt-backend/install.sh
+++ nwt-backend/install.sh
@@ -4,6 +4,3 @@ sudo apt install python3.9 python3.9-venv
 python3.9 --version
 python3.9 -m venv venv
 source ./venv/bin/activate
-pip3.9 install -U pip
-pip3.9 install -U setuptools wheel
-pip3.9 install -r requirements.txt
