68305973f755952372f3f5ae8df8379a5c774d2f
picroc <picroc@yandex-team.ru>
2020-12-13 19:27:36 +0300

    feat: add api service

--- net-worth-tracker/src/services/Api/Api.ts
+++ net-worth-tracker/src/services/Api/Api.ts
@@ -1,1 +1,80 @@
-export class Api { }
+import { env } from "process";
+
+enum RequestMethod {
+    GET = 'GET',
+    POST = 'POST'
+}
+
+enum RecordType {
+    LIABILITY = 'liability',
+    ASSET = 'asset'
+}
+
+interface Record {
+    datetime: Date;
+    name: string;
+    type: RecordType;
+    amount: number;
+}
+
+interface TotalInfo {
+    asset: number;
+    liability: number;
+}
+
+interface RequestOptions {
+    method?: RequestMethod;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    body?: any;
+}
+
+export class Api { 
+    baseUrl: string = env.BASE_URL || "http://localhost:8000";
+    loggedInUser = {};
+
+    fetchApi = async <T>(
+        url: string,
+        options: RequestOptions
+    ): Promise<T> => {
+        const {method, body} = options;
+        console.log('Settin body', body);
+        const result = await fetch(`${this.baseUrl}${url}`, {
+            body: JSON.stringify(body),
+            method
+        });
+        if (result.ok) {
+            return method === RequestMethod.GET ? await result.json() as T : {} as T;
+        }
+        throw new Error(await result.text());
+    };
+
+    public login = (username: string, password: string): Promise<void> =>
+        this.fetchApi('/login/', {
+            method: RequestMethod.POST,
+            body: {
+                username,
+                password
+            }
+        });
+
+    public logout = (): Promise<void> =>
+        this.fetchApi('/logout/', {
+            method: RequestMethod.GET
+        });
+
+    public getTotal = (): Promise<TotalInfo> =>
+        this.fetchApi('/api-total/', {
+            method: RequestMethod.GET
+        });
+
+    public getRecords = (): Promise<Array<Record>>  =>
+        this.fetchApi('/api-records/', {
+            method: RequestMethod.GET
+        })
+
+    public addRecord = (record: Record): Promise<void> => 
+        this.fetchApi('/api-records/', {
+            method: RequestMethod.POST,
+            body: record
+        })
+}
--- nwt-backend/backend/settings.py
+++ nwt-backend/backend/settings.py
@@ -38,13 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -115,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS=['http://localhost:3000']
+               
+CORS_ALLOW_ALL_ORIGINS = True
